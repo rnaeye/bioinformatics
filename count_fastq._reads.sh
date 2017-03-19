43675ac7a420313847651f992ad4f51b255ea40d
Erbay Yigit <erbayyigit@gmail.com>
2017-03-19 14:44:24 -0400

    removed bc -l

--- count_fastq._reads.sh
+++ count_fastq._reads.sh
@@ -1,9 +1,12 @@
-# Erbay Yigit. 
-# count reads in fastq.gz file in a directory
+# Author: Erbay Yigit. 
+# Date: March 19, 2017
+# Count reads in fastq.gz file in a directory
 # USAGE: <bash> <this_script.sh> <path_to_runFolder/AB9EG>
-# Count only R1. R2 will have the same number of reads
+# Counts only R1. R2 will have the same number of reads
+
 for i in $1/*.1.fastq.gz 
 do
     file_name = $(basename -s 1.fastq.gz $i)
-    echo "${file_name}" $(gunzip -c $i | echo $(wc -l)/4 | bc -l)
+    echo "${file_name}" $(gunzip -c $i | echo $(wc -l)/4)
 done
+
